1e8a708a34b650c1c2833d8bbceae85a2db54954
Richard Lee <Rich@Richard-Lees-MacBook-Pro.local>
2012-01-10 16:07:09 +1100

    Initial Commit

---
+++ Easter ShowTests/Easter_ShowTests.h
@@ -0,0 +1,13 @@
+//
+//  Easter_ShowTests.h
+//  Easter ShowTests
+//
+//  Created by Richard Lee on 10/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Easter_ShowTests : SenTestCase
+
+@end
